91ec7867a1ac3052206644e3641f320ecc67961f
Srteixeira1992 <101927353+Srteixeira1992@users.noreply.github.com>
2024-11-12 11:23:10 +0000

    Adicionar exercício 1

---
+++ exercicio1.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Caminho para o diretório
+cd Citeforma_Análise_de_Dados
+
+cd "10806 - Linguagens de scripting e linha de comandos"
+
+cd "Aula 4"
+
+cd Ex1---ScriptingCiteforma2004
+
+# Listar
+ls
+exercicio1.sh  README.md
+
+# Criar ficheros .txt
+touch teste1.txt teste2.txt
+
+# Criar diretório backup
+mkdir backup
+
+# Mover ficheiros para pasta backup 
+mv *.txt backup/
