fed903d3205d10bd119f582c32d261377b1fa6da
Srteixeira1992 <101927353+Srteixeira1992@users.noreply.github.com>
2024-11-13 11:30:07 +0000

    Update exercicio2.sh

--- exercicio2.sh
+++ exercicio2.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 
-# Contar linhas e palavras
-wc -l -w backup/teste1.txt
+# Verifica se o ficheiro foi fornecido como argumento
+if [ -z "$1" ]; then
+  echo "Por favor, forneça o caminho de um ficheiro como argumento."
+  exit 1
+fi
 
-# Mostrar as 5 primeiras linhas
-head -n 5 backup/teste1.txt
+# Verifica se o ficheiro especificado existe
+ficheiro="$1"
+if [ ! -f "$ficheiro" ]; then
+  echo "O ficheiro especificado não existe."
+  exit 1
+fi
 
-# Filtra linhas com "erro"
-grep -i "erro" backup/teste1.txt
+# Contar o número de linhas e palavras do ficheiro
+num_linhas=$(wc -l < "$ficheiro")
+num_palavras=$(wc -w < "$ficheiro")
+echo "O ficheiro '$ficheiro' contém $num_linhas linhas e $num_palavras palavras."
 
+# Mostrar apenas as primeiras 5 linhas do ficheiro
+echo "As primeiras 5 linhas do ficheiro são:"
+head -n 5 "$ficheiro"
 
-git add exercicio2.sh
-git commit -am "Adicionar execicio 2"
-git push
+# Filtrar e mostrar todas as linhas que contenham a palavra "erro" (case insensitive)
+echo "Linhas que contêm a palavra 'erro' (independente de maiúsculas/minúsculas):"
+grep -i "erro" "$ficheiro"
